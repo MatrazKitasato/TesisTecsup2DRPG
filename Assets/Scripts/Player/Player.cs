7713d2fb00ff785ea9160748915cec795599e2f0
MatrazKitasato <carlos.tome@tecsup.edu.pe>
2024-10-19 14:20:54 -0500

    Animation&Movement

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] float speed;
+    [SerializeField] float moveX;
+    [SerializeField] float moveY;
+    [SerializeField] Vector2 movement;
+
+    [Header("Animation")]
+    [SerializeField] Animator anim;
+
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+        UpdateAnimation();
+    }
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        if(movement.magnitude > 0)
+        {
+            rb.velocity = movement.normalized * speed * Time.fixedDeltaTime;
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+            Debug.Log("No se mueve");
+        }
+        Debug.Log("velocidad:" + movement);
+        Debug.Log("magnitud" + movement.magnitude);
+    }
+    public void GetInput()
+    {
+        moveX = Input.GetAxis("Horizontal");
+        moveY = Input.GetAxis("Vertical");
+
+        movement = new Vector2(moveX, moveY);
+    }
+    public void UpdateAnimation()
+    {
+        if(movement.magnitude > 0)
+        {
+            anim.SetBool("Walk", true);
+            anim.SetFloat("MoveX", moveX);
+            anim.SetFloat("MoveY", moveY);
+        }
+        else
+        {
+            anim.SetBool("Walk", false);
+        }
+        
+    }
+}
