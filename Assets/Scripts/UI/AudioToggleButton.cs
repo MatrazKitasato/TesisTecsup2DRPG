32e3d2ade2049461b7212a546d6018481d225043
MatrazKitasato <carlos.tome@tecsup.edu.pe>
2024-10-22 12:08:06 -0500

    SceneAndSound(almost)

---
+++ Assets/Scripts/UI/AudioToggleButton.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioToggleButton : MonoBehaviour
+{
+    public Button targetButton;
+    public SpriteAudioManager audioManager;
+    void Start()
+    {
+        audioManager = FindObjectOfType<SpriteAudioManager>();
+
+        if (audioManager == null)
+        {
+            Debug.LogError("No se encontró un SpriteAudioManager en la escena.");
+            return;
+        }
+        targetButton.onClick.AddListener(audioManager.ToggleAudioAndSprite);
+    }
+}
---
+++ Assets/Scripts/UI/SpriteAudioManager.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpriteAudioManager : MonoBehaviour
+{
+    [Header("Sprites")]
+    public Image spriteRenderer;
+    public Sprite spriteOn;
+    public Sprite spriteOff;
+    public Button targetButton;
+    [Header("Sounds")]
+    public AudioSource audioSource;
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        int isMuted = PlayerPrefs.GetInt("isAudioMuted", 0);
+        UpdateSpriteAndAudio(isMuted == 1);
+    }
+
+    public void ToggleAudioAndSprite()
+    {
+        bool isCurrentlyMuted = audioSource.mute;
+
+        UpdateSpriteAndAudio(!isCurrentlyMuted);
+
+        PlayerPrefs.SetInt("isAudioMuted", isCurrentlyMuted? 0 : 1);
+        PlayerPrefs.Save();
+    }
+
+    private void UpdateSpriteAndAudio(bool mute)
+    {
+        Debug.Log("Actualizando sprite y audio: " + (mute ? "Mute" : "On"));
+        audioSource.mute = mute;
+        targetButton.image.sprite = mute ? spriteOff : spriteOn;
+        Debug.Log("Sprite actualizado a: " + spriteRenderer.sprite.name);
+    }
+}
