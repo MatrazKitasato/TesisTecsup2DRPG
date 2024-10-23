a74574115decdecffa9e9b3464fc37845b664f40
MatrazKitasato <carlos.tome@tecsup.edu.pe>
2024-10-23 15:28:42 -0500

    DialogueDone

--- Assets/Scripts/NPC/Dialogue.cs
+++ Assets/Scripts/NPC/Dialogue.cs
@@ -1,29 +1,84 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Dialogue : MonoBehaviour
 {
     [SerializeField] bool isPlayerInRange;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] bool didDialogueStart;
+    [SerializeField] int lineIndex;
+    [SerializeField] GameObject exclamationMark;
+    [SerializeField] GameObject dialoguePanel;
+    [SerializeField] TMP_Text dialogueText;
+    [SerializeField, TextArea(4,6)] string[] dialogueLines;
+    [SerializeField] float typingTime = 0.05f;
+    
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(isPlayerInRange && Input.GetButtonDown("Fire1"))
+        {
+            if (!didDialogueStart)
+            {
+                StartDialogue();
+            }
+            else if(dialogueText.text == dialogueLines[lineIndex])
+            {
+                NexDialogueLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                dialogueText.text = dialogueLines[lineIndex];
+            }
+        }
+    }
+    private void StartDialogue()
+    {
+        didDialogueStart = true;
+        exclamationMark.SetActive(false);
+        dialoguePanel.SetActive(true);
+        lineIndex = 0;
+        Time.timeScale = 0f;
+        StartCoroutine(ShowLine());
+    }
+    private void NexDialogueLine()
+    {
+        lineIndex++;
+        if(lineIndex < dialogueLines.Length)
+        {
+            StartCoroutine(ShowLine());
+        }
+        else
+        {
+            didDialogueStart = false;
+            dialoguePanel.SetActive(false);
+            exclamationMark?.SetActive(true);
+            Time.timeScale = 1f;
+        }
+    }
+    private IEnumerator ShowLine()
+    {
+        dialogueText.text = string.Empty;
+
+        foreach (char ch in dialogueLines[lineIndex])
+        {
+            dialogueText.text += ch;
+            yield return new WaitForSecondsRealtime(typingTime);
+        }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         isPlayerInRange = true;
+        exclamationMark.SetActive(true);
         Debug.Log("Se puede iniciar dialogo");
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
         isPlayerInRange = false;
+        exclamationMark.SetActive(false);
         Debug.Log("No se puede iniciar dialogo");
     }
 }
